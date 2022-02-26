437d103b846fdde9468c107778493fb7405ef6f5
Samuel Gonzalez <samglz10@gmail.com>
2022-02-25 23:47:08 -0500

    Incomplete

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // Your Code Here
 let userName = window.prompt('Please enter your name:')
+for (let i = 0; i < questions.length; i++) {
+    let question = (questions[i])
+    let userAnswer = window.prompt(question.text)
+    if (userAnswer === question.correctAnswer) {
+        console.log( 'Correct' )
+    }
+}
+
